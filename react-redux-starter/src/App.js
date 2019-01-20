af98f02a262e5cfe2e41b5b163f5afc3f3d233f8
Ei Maung <eimg@fairwayweb.com>
2019-01-20 15:59:34 +0630

    added react router

--- react-redux-starter/src/App.js
+++ react-redux-starter/src/App.js
@@ -1,39 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-class List extends Component {
-    render() {
-        return (
-            <ul>
-                {this.props.items.map((subject) => {
-                    return <li>{subject}</li>
-                })}
-            </ul>
-        )
-    }
+const List = (props) => {
+    return (
+        <ul>
+            {props.items.map((subject) => {
+                return <li>{subject}</li>
+            })}
+        </ul>
+    )
 }
 
-class App extends Component {
-    constructor() {
-        super();
-        this.input = React.createRef();
-        this.add = this.add.bind(this);
-    }
+const App = (props) => {
+    let input = React.createRef();
 
-    add() {
-        var subject = this.input.current.value;
-        this.props.add(subject);
-    }
-
-    render() {
-        return (
-            <div>
-                <input type="text" ref={this.input} />
-                <button onClick={this.add}>+</button>
-                <List items={this.props.state} />
-            </div>
-        );
-    }
+    return (
+        <div>
+            <input type="text" ref={input} />
+            <button onClick={() => {
+                var subject = input.current.value;
+                props.add(subject);
+            }}>+</button>
+            <List items={props.state} />
+        </div>
+    );
 }
 
 function state(state) {
